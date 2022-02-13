10c3183bc726baccbfcc79527a71695102ba556a
Alexey Zhokhov <alexey@zhokhov.com>
2022-02-14 05:07:25 +1000

    Removed sonar plugin.

--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -13,8 +13,6 @@ repositories {
 dependencies {
     // https://plugins.gradle.org/plugin/com.github.ben-manes.versions
     implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
-    // https://plugins.gradle.org/plugin/org.sonarqube
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3")
     // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     // https://plugins.gradle.org/plugin/com.diffplug.spotless
