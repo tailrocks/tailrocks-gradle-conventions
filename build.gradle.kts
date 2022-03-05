2590e2e15483adcb639d716bf49c71e7a41e2732
Alexey Zhokhov <alexey@zhokhov.com>
2022-03-05 20:33:37 +0800

    Spotless apply.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 
     // https://plugins.gradle.org/plugin/com.gradle.plugin-publish
     id("com.gradle.plugin-publish") version "0.20.0"
+
+    // https://plugins.gradle.org/plugin/com.diffplug.spotless
+    id("com.diffplug.spotless") version "6.3.0"
 }
 
 val javaVersion = 17
@@ -16,6 +19,26 @@ java {
 allprojects {
     group = "com.tailrocks.gradle"
     version = "0.1.0"
+
+    apply(plugin = "com.diffplug.spotless")
+
+    spotless {
+        java {
+            removeUnusedImports()
+            trimTrailingWhitespace()
+            endWithNewline()
+            targetExclude("**/generated/**")
+        }
+        kotlinGradle {
+            ktlint()
+        }
+        java {
+            licenseHeaderFile("$rootDir/gradle/licenseHeader.txt")
+        }
+        kotlin {
+            licenseHeaderFile("$rootDir/gradle/licenseHeader.txt")
+        }
+    }
 }
 
 pluginBundle {
